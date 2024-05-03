244008612d9cca550d935874cc79b7727a915993
dannnnnm <137915605+dannnnnm@users.noreply.github.com>
2024-05-03 15:08:22 -0400

    tutorial status

---
+++ src/components/ComponenteTutorial.vue
@@ -0,0 +1,40 @@
+
+
+<template>
+    <div>
+        <h1>{{ titulo }}</h1>
+        <button @click="incrementar">Incrementar</button>
+        <br>
+        <br>
+        <button @click="restar">Restar</button>
+        <p>Contador: {{ contador }}</p>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+
+
+    const titulo="Componente con setup"
+    const contador=ref(0)
+
+    function incrementar() {
+        contador.value++
+    }
+
+    function restar(){
+        contador.value--
+    }
+
+</script>
+
+<style scoped>
+    h1{
+        color: blue;
+
+    }
+    p{
+        color: green;
+    }
+</style>
